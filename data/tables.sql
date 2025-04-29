273a555f82cce0190307313fcf0dca0082b14f3f
Sean Lyons <lyonss@carleton.edu>
2025-04-29 14:16:36 -0500

    Group work April 29, 2025

--- data/tables.sql
+++ data/tables.sql
@@ -1,19 +1,95 @@
-CREATE TABLE authors (
-    id integer NOT NULL,
-    surname text,
-    given_name text,
-    birth_year integer,
-    death_year integer
+-- This file contains the SQL commands to create the tables for the tsunami database.
+CREATE TABLE tsunami_attribute (
+   SOURCE_ID integer NOT NULL,
+   WAVE_ID integer NOT NULL,,
+   DISTANCE_FROM_SOURCE integer NOT NULL,
+   TRAVEL_TIME_HOURS integer,
+   TRAVEL_TIME_MINUTES integer,
+   VALIDITY integer,
+   MEASUREMENT_TYPE integer,
+   wave_PERIOD integer,
+   FIRST_MOTION integer,
+   MAXIMUM_HEIGHT real,
+   HORIZONRTAL_INNUNDATION integer,
+);
+CREATE TABLE tsunami_destruction (
+   WAVE_ID integer NOT NULL,
+   INJURIES integer,
+   INJURY_ESTIMATE integer,
+   FATALITIES integer,
+   FATALITY_ESTIMATE integer,
+   DAMAGE_MILLIONS_DOLLARS integer,
+   DAMAGE_ESTIMATE integer,
+   HOUSES_DAMAGED integer,
+   HOUSE_DAMAGE_ESTIMATE integer,
+   HOUSES_DESTROYED integer,
+   HOUSE_DESTRUCTION_ESTIMATE integer,
 );
 
-CREATE TABLE books (
-    id integer NOT NULL,
-    title text,
-    publication_year integer
+CREATE TABLE tsunami_place_time (
+   WAVE_ID integer NOT NULL,
+   wave_YEAR integer NOT NULL,
+   wave_MONTH text,
+   wave_DAY integer,
+   REGION_CODE integer,
+   COUNTRY text,
+   wave_STATE/PROVINCE text,
+   wave_LOCATION text,
+   LATITUDE real NOT NULL,
+   LONGITUDE real NOT NULL,
 );
 
-CREATE TABLE books_authors (
-    book_id integer,
-    author_id integer
+CREATE TABLE tsunami_ids (
+   SOURCE_ID integer NOT NULL,
+   WAVE_ID integer NOT NULL,
 );
+
+-- CREATE TABLE sources (
+-- SOURCE_ID integer NOT NULL,
+-- SOURCE_YEAR integer,
+-- SOURCE_MONTH integer,
+-- SOURCE_HOUR integer,MINUTE integer,
+-- CAUSE integer,
+-- VALIDITY integer,
+-- FOCAL_DEPTH integer,
+-- PRIMARY_MAGNITUDE real,
+-- REGION_CODE integer,
+-- COUNTRY text,
+-- STATE_PROVINCE text,
+-- source_LOCATION text,
+-- LATITUDE real,
+-- LONGITUDE real,
+-- MAXIMUM_HEIGHT real,
+-- MAGNITUDE_ABE real,
+-- MAGNITUDE_IIDA real,
+-- INTENSITY_SOLOVIEV integer,
+-- WARNING_STATUS integer,
+-- MISSING integer,
+-- MISSING_ESTIMATE integer,
+-- INJURIES integer,
+-- INJURY_ESTIMATE integer,
+-- FATALITIES integer,
+-- FATALITY_ESTIMATE integer,
+-- DAMAGE_MILLIONS_DOLLARS real,
+-- DAMAGE_ESTIMATE integer,
+-- HOUSES_DAMAGED integer,
+-- HOUSE_DAMAGE_ESTIMATE integer,
+-- HOUSES_DESTROYED integer,
+-- HOUSE_DESTRUCTION_ESTIMATE integer,
+-- ALL_MISSING integer,
+-- MISSING_TOTAL integer,
+-- ALL_INJURIES integer,
+-- INJURY_TOTAL integer,
+-- ALL_FATALITIES integer,
+-- FATALITY_TOTAL integer,
+-- ALL_DAMAGE_MILLIONS real,
+-- DAMAGE_TOTAL integer,
+-- ALL_HOUSES_DAMAGED integer,
+-- HOUSE_DAMAGE_TOTAL integer,
+-- ALL_HOUSES_DESTROYED integer
+-- );
+
+-- CREATE TABLE source_wave (
+--     wave_id integer
+-- );
 
