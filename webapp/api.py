51230654db517984ea55d41bc7fa24317cb7e558
Seanlyons316 <lyonss@carleton.edu>
2025-05-09 15:00:21 -0400

    Saturday assignment updates

--- webapp/api.py
+++ webapp/api.py
@@ -8,7 +8,7 @@
     database like the one I've used in CS257 for the past few years,
     including an authors table with fields
 
-        (id, given_name, surname, birth_year, death_year)
+        (id, given_name, surname, birth_year, fatality_year)
 
     You might also want to consult the official psycopg2 tutorial
     at https://wiki.postgresql.org/wiki/Psycopg2_Tutorial.
@@ -53,13 +53,20 @@ def get_tsunamis():
     try:
         connection = get_connection()
         cursor = connection.cursor()
-        query = '''SELECT * FROM tsunamis_attribute, tsunamis_destruction, tsunamis_place_time
-        WHERE tsunamis_attribute.WAVE_ID = tsunamis_destruction.WAVE_ID 
-        AND tsunamis_destruction.WAVE_ID = tsunamis_place_time.WAVE_ID 
-        ORDER BY wave_YEAR DESC
+        query = '''SELECT ta.source_id, ta.WAVE_ID, ta.distance_from_source, ta.travel_time_hours,
+        ta.validity, ta.measurement_type, ta.wave_period, ta.first_motion,
+        ta.maximum_height, ta.horizonrtal_innundation,
+        td.injuries, td.injury_estimate, td.fatalities, td.fatality_estimate,
+        td.houses_damaged, td.house_damage_estimate,
+        td.houses_destroyed, td.house_destruction_estimate,
+        tp.region_code, tp.country, tp.wave_year, tp.wave_month, tp.wave_day,
+        tp.wave_state, tp.wave_location, tp.latitude, tp.longitude 
+        FROM tsunamis_attribute AS ta, tsunamis_destruction AS td, tsunamis_place_time AS tp
+        WHERE ta.WAVE_ID = td.WAVE_ID 
+        AND td.WAVE_ID = tp.WAVE_ID 
+        ORDER BY tp.wave_YEAR DESC
         LIMIT 10'''
         cursor.execute(query)
-
         for row in cursor:
             tsunamis.append({'source id': row[0],
                             'wave id': row[1],
@@ -70,11 +77,11 @@ def get_tsunamis():
                             'wave period': row[6],
                             'first motion': row[7],
                             'max_height': row[8],
-                            'horizontal innundation': row[9],
+                            'horizonrtal innundation': row[9],
                             'injuries': row[10],
                             'injury estimate': row[11],
-                            'deaths': row[12],
-                            'death estimate': row[13],
+                            'fatalities': row[12],
+                            'fatality estimate': row[13],
                             'houses damaged': row[14],
                             'houses damaged estimate': row[15],
                             'houses destroyed': row[16],
@@ -108,15 +115,22 @@ def get_tsunamis_by_country():
     else:
         return 'Please provide a country name.'
     try:
-        query = '''SELECT * FROM tsunamis_attribute, tsunamis_destruction, tsunamis_place_time
-        WHERE tsunamis_attribute.WAVE_ID = tsunamis_destruction.WAVE_ID 
-        AND tsunamis_destruction.WAVE_ID = tsunamis_place_time.WAVE_ID 
-        AND tsunamis_place_time.country = %s
-        ORDER BY wave_YEAR DESC'''
+        query = '''SELECT ta.source_id, ta.WAVE_ID, ta.distance_from_source, ta.travel_time_hours,
+        ta.validity, ta.measurement_type, ta.wave_period, ta.first_motion,
+        ta.maximum_height, ta.horizonrtal_innundation,
+        td.injuries, td.injury_estimate, td.fatalities, td.fatality_estimate,
+        td.houses_damaged, td.house_damage_estimate,
+        td.houses_destroyed, td.house_destruction_estimate,
+        tp.region_code, tp.country, tp.wave_year, tp.wave_month, tp.wave_day,
+        tp.wave_state, tp.wave_location, tp.latitude, tp.longitude 
+        FROM tsunamis_attribute AS ta, tsunamis_destruction AS td, tsunamis_place_time AS tp
+        WHERE ta.WAVE_ID = td.WAVE_ID 
+        AND td.WAVE_ID = tp.WAVE_ID 
+        AND tp.country = %s
+        ORDER BY tp.wave_YEAR DESC'''
         connection = get_connection()
         cursor = connection.cursor()
         cursor.execute(query, parameters)
-        print(cursor.query)
         for row in cursor:
             tsunamis.append({'source id': row[0],
                             'wave id': row[1],
@@ -127,11 +141,11 @@ def get_tsunamis_by_country():
                             'wave period': row[6],
                             'first motion': row[7],
                             'max_height': row[8],
-                            'horizontal innundation': row[9],
+                            'horizonrtal innundation': row[9],
                             'injuries': row[10],
                             'injury estimate': row[11],
-                            'deaths': row[12],
-                            'death estimate': row[13],
+                            'fatalities': row[12],
+                            'fatality estimate': row[13],
                             'houses damaged': row[14],
                             'houses damaged estimate': row[15],
                             'houses destroyed': row[16],
@@ -163,10 +177,18 @@ def get_tsunamis_by_wave_id():
     else:
         return 'Please provide a wave id.'
     try:
-        query = '''SELECT * FROM tsunamis_attribute, tsunamis_destruction, tsunamis_place_time
-        WHERE tsunamis_attribute.WAVE_ID = tsunamis_destruction.WAVE_ID
-        AND tsunamis_destruction.WAVE_ID = tsunamis_place_time.WAVE_ID
-        AND tsunamis_attribute.WAVE_ID = %s'''
+        query = '''SELECT ta.source_id, ta.WAVE_ID, ta.distance_from_source, ta.travel_time_hours,
+        ta.validity, ta.measurement_type, ta.wave_period, ta.first_motion,
+        ta.maximum_height, ta.horizonrtal_innundation,
+        td.injuries, td.injury_estimate, td.fatalities, td.fatality_estimate,
+        td.houses_damaged, td.house_damage_estimate,
+        td.houses_destroyed, td.house_destruction_estimate,
+        tp.region_code, tp.country, tp.wave_year, tp.wave_month, tp.wave_day,
+        tp.wave_state, tp.wave_location, tp.latitude, tp.longitude 
+        FROM tsunamis_attribute AS ta, tsunamis_destruction AS td, tsunamis_place_time AS tp
+        WHERE ta.WAVE_ID = td.WAVE_ID 
+        AND td.WAVE_ID = tp.WAVE_ID 
+        AND ta.WAVE_ID = %s'''
         connection = get_connection()
         cursor = connection.cursor()
         cursor.execute(query, parameters)
@@ -180,11 +202,11 @@ def get_tsunamis_by_wave_id():
                             'wave period': row[6],
                             'first motion': row[7],
                             'max_height': row[8],
-                            'horizontal innundation': row[9],
+                            'horizonrtal innundation': row[9],
                             'injuries': row[10],
                             'injury estimate': row[11],
-                            'deaths': row[12],
-                            'death estimate': row[13],
+                            'fatalities': row[12],
+                            'fatality estimate': row[13],
                             'houses damaged': row[14],
                             'houses damaged estimate': row[15],
                             'houses destroyed': row[16],
@@ -232,16 +254,23 @@ def get_tsunamis_by_year_range():
     if before < -2000 or after > 2023:
         return 'Please provide a start year that is greater than -2000 and an end year that is less than 2023.'
     try:
-        query = '''SELECT * FROM tsunamis_attribute, tsunamis_destruction, tsunamis_place_time
-        WHERE tsunamis_attribute.WAVE_ID = tsunamis_destruction.WAVE_ID 
-        AND tsunamis_destruction.WAVE_ID = tsunamis_place_time.WAVE_ID 
-        AND tsunamis_place_time.wave_YEAR BETWEEN %s AND %s
-        ORDER BY wave_YEAR DESC'''
+        query = '''SELECT ta.source_id, ta.WAVE_ID, ta.distance_from_source, ta.travel_time_hours,
+        ta.validity, ta.measurement_type, ta.wave_period, ta.first_motion,
+        ta.maximum_height, ta.horizonrtal_innundation,
+        td.injuries, td.injury_estimate, td.fatalities, td.fatality_estimate,
+        td.houses_damaged, td.house_damage_estimate,
+        td.houses_destroyed, td.house_destruction_estimate,
+        tp.region_code, tp.country, tp.wave_year, tp.wave_month, tp.wave_day,
+        tp.wave_state, tp.wave_location, tp.latitude, tp.longitude 
+        FROM tsunamis_attribute AS ta, tsunamis_destruction AS td, tsunamis_place_time AS tp
+        WHERE ta.WAVE_ID = td.WAVE_ID 
+        AND td.WAVE_ID = tp.WAVE_ID 
+        AND CAST(tp.wave_YEAR AS FLOAT) BETWEEN %s AND %s
+        ORDER BY tp.wave_YEAR DESC'''
 
         connection = get_connection()
         cursor = connection.cursor()
         cursor.execute(query, parameters)
-        print(cursor.query)
         for row in cursor:
             tsunamis.append({'source id': row[0],
                             'wave id': row[1],
@@ -252,11 +281,11 @@ def get_tsunamis_by_year_range():
                             'wave period': row[6],
                             'first motion': row[7],
                             'max_height': row[8],
-                            'horizontal innundation': row[9],
+                            'horizonrtal innundation': row[9],
                             'injuries': row[10],
                             'injury estimate': row[11],
-                            'deaths': row[12],
-                            'death estimate': row[13],
+                            'fatalities': row[12],
+                            'fatality estimate': row[13],
                             'houses damaged': row[14],
                             'houses damaged estimate': row[15],
                             'houses destroyed': row[16],
@@ -276,7 +305,7 @@ def get_tsunamis_by_year_range():
         print(e, file=sys.stderr)
 
     connection.close()
-    return json.dumps(tsunamis, indent = 4)
+    return json.dumps(tsunamis[:100], indent = 4)
 
 @app.route('/tsunamis/country_years', methods=['GET'])
 def get_tsunamis_by_country_and_year_range():
@@ -286,12 +315,20 @@ def get_tsunamis_by_country_and_year_range():
     tsunamis = []
     print(type(tsunamis))
     try:
-        query = '''SELECT * FROM tsunamis_attribute, tsunamis_destruction, tsunamis_place_time
-        WHERE tsunamis_attribute.WAVE_ID = tsunamis_destruction.WAVE_ID 
-        AND tsunamis_destruction.WAVE_ID = tsunamis_place_time.WAVE_ID 
-        AND tsunamis_place_time.wave_year BETWEEN %s AND %s
-        AND tsunamis_place_time.country = %s
-        ORDER BY wave_YEAR DESC'''
+        query = '''SELECT ta.source_id, ta.WAVE_ID, ta.distance_from_source, ta.travel_time_hours,
+        ta.validity, ta.measurement_type, ta.wave_period, ta.first_motion,
+        ta.maximum_height, ta.horizonrtal_innundation,
+        td.injuries, td.injury_estimate, td.fatalities, td.fatality_estimate,
+        td.houses_damaged, td.house_damage_estimate,
+        td.houses_destroyed, td.house_destruction_estimate,
+        tp.region_code, tp.country, tp.wave_year, tp.wave_month, tp.wave_day,
+        tp.wave_state, tp.wave_location, tp.latitude, tp.longitude 
+        FROM tsunamis_attribute AS ta, tsunamis_destruction AS td, tsunamis_place_time AS tp
+        WHERE ta.WAVE_ID = td.WAVE_ID 
+        AND td.WAVE_ID = tp.WAVE_ID 
+        AND tp.wave_year BETWEEN %s AND %s
+        AND tp.country = %s
+        ORDER BY tp.wave_YEAR DESC'''
         connection = get_connection()
         cursor = connection.cursor()
         cursor.execute(query, (start_year, end_year, country))
@@ -305,11 +342,11 @@ def get_tsunamis_by_country_and_year_range():
                             'wave period': row[6],
                             'first motion': row[7],
                             'max_height': row[8],
-                            'horizontal innundation': row[9],
+                            'horizonrtal innundation': row[9],
                             'injuries': row[10],
                             'injury estimate': row[11],
-                            'deaths': row[12],
-                            'death estimate': row[13],
+                            'fatalities': row[12],
+                            'fatality estimate': row[13],
                             'houses damaged': row[14],
                             'houses damaged estimate': row[15],
                             'houses destroyed': row[16],
